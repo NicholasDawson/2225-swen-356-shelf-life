7539e62ec7e21ade1893383ccaf7b95f3b4dd03b
Jarred Moyer <jarredds941@gmail.com>
2023-02-27 09:03:47 -0500

    initial styling

--- frontend/src/components/signup/SignUp.js
+++ frontend/src/components/signup/SignUp.js
@@ -10,14 +10,31 @@ class SignUp extends Component {
 
     render() {
         return (
-            <div className="signup">
-            <div className="cat-absolute-container">
-                <button className="enter-username">Enter Username</button>
-                <button className="enter-password">Enter Password</button>
-                <button className="confirm-password">Confirm Password</button>
-                <button className="sign-up">Sign Up</button>
-            </div>
-            </div>
+            <body class="form-cont">
+
+                <main class="form-signin w-100 m-auto">
+                <form>
+                    <h1 class="h3 mb-3 fw-normal">Sign Up</h1>
+
+                    <div class="form-floating">
+                        
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"></input>
+                        <label for="floatingInput">Email address: </label>
+                    </div>
+                    <div class="form-floating">
+                        
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password"></input>
+                        <label for="floatingPassword">Password: </label>
+                    </div>
+                    <div class="form-floating">
+                        
+                        <input type="password" class="form-control" id="floatingPasswordC" placeholder="Password"></input>
+                        <label for="floatingPasswordC">Confirm Password: </label>
+                    </div>
+                    <button class="w-100 btn btn-lg btn-primary b_color" type="submit">Sign Up</button>
+                </form>
+                </main>
+            </body>
         )
     }
 }
