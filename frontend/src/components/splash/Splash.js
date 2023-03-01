7180a5c6bd80441d08a6394b5ee3df21ec0b2d80
Jarred Moyer <jarredds941@gmail.com>
2023-03-01 09:15:16 -0500

    new splace with bootstrap

--- frontend/src/components/splash/Splash.js
+++ frontend/src/components/splash/Splash.js
@@ -1,6 +1,9 @@
 import { Component } from "react";
 import './splash.css'
 import * as React from "react";
+import food from './food.svg';
+import receipt from './receipt.svg'
+import empty from './shelf_empty.svg'
 //import featuresWeb3 from "./assets/featuresWeb3.svg";
 class Splash extends Component {
     constructor(props){
@@ -22,7 +25,7 @@ class Splash extends Component {
               <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
                 <div class="feature col">
                   <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                    
+                    <img src={food} alt="" height="36em" width="36em"></img>
                   </div>
                   <h3 class="fs-2">Featured title</h3>
                   <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
@@ -32,7 +35,7 @@ class Splash extends Component {
                 </div>
                 <div class="feature col">
                   <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                    
+                    <img src={receipt} alt="" height="36em" width="36em"></img>
                   </div>
                   <h3 class="fs-2">Featured title</h3>
                   <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
@@ -42,7 +45,7 @@ class Splash extends Component {
                 </div>
                 <div class="feature col">
                   <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                    
+                    <img src={empty} alt="" height="36em" width="36em"></img>
                   </div>
                   <h3 class="fs-2">Featured title</h3>
                   <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
