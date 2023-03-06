a17dc577cfc52d611b3c84c1fa7a7a991e7a0151
Nick Dawson <37987430+NicholasDawson@users.noreply.github.com>
2023-03-06 08:42:38 -0500

    Fix router for prod

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
+import "./App.css";
 import Splash from "./components/splash/Splash.js";
 import Signup from "./components/signup/SignUp";
 import Login from "./components/login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/2225-swen-356-shelf-life">
       <Routes>
-          <Route path="/splash" element={<Splash></Splash>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+        <Route path="/splash" element={<Splash></Splash>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
