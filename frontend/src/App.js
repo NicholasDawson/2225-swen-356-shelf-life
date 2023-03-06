dfbbba71e937cd7fcb2b4cbaa4f9ecc68e5f91f8
Nick Dawson <37987430+NicholasDawson@users.noreply.github.com>
2023-03-06 08:51:53 -0500

    Fixed router again...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,17 @@ import "./App.css";
 import Splash from "./components/splash/Splash.js";
 import Signup from "./components/signup/SignUp";
 import Login from "./components/login/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <BrowserRouter basename="/2225-swen-356-shelf-life">
+    <HashRouter>
       <Routes>
+        <Route path="/" element={<Splash />} />
         <Route path="/splash" element={<Splash></Splash>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
